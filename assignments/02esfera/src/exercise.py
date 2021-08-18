a7369333f2226050da75beddfc8ed634f90b0ca7
Fidel-Uriarte <fidel.um@culiacan.tecnm.mx>
2021-08-18 09:27:29 +0000

    ejercicio esfera agregando área.

--- assignments/02esfera/src/exercise.py
+++ assignments/02esfera/src/exercise.py
@@ -2,8 +2,9 @@ def main():
     #escribe tu código abajo de esta línea
     import math
     r=float(input("dame el radio de la esfera: "))
+    area=float(4*math.pi*r**2)
     volumen=float((4/3)*math.pi*math.pow(r,3))
     print("El volumen de la esfera es: ", volumen)
-
+    print("El area de la esfera es: ", area)
 if __name__=='__main__':
     main()
