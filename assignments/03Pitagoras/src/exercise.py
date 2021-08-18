44d4aaefd21bcf37c2ede2fb39e5dc48a8a4136e
Fidel-Uriarte <fidel.um@culiacan.tecnm.mx>
2021-08-18 09:19:41 +0000

    03Pitagoras

---
+++ assignments/03Pitagoras/src/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    #escribe tu código abajo de esta línea
+    import math
+    a=float(input("Dame el valor de a:"))
+    b=float(input("Dame el valor de b:"))
+    c=math.sqrt(a**2+b**2)
+    
+    print("El valor de hipotenusa es:", c)
+    
+if __name__=='__main__':
+    main()
